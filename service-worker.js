9cfaee17bb0e4145eddb9a3de82e0bcc651afdff
sawon-21 <businessmansawon@gmail.com>
2025-04-08 16:23:48 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+// Cache names
+const CACHE_NAME = 'my-cache-v';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/https://quran.com/bn'
+  '/Quran.jpg',
+  '/poster.jpg',
+  'https://fonts.googleapis.com/css?family=Noto+Sans+Bengali:300,400,700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
+  'https://fonts.maateen.me/apona-lohit/font.css'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
